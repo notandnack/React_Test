2a58469d847b4c2f958d1d07bd732ac6bc54f4d8
ศิวกร คุณวงศ์ <notandnack@hotmail.com>
2022-07-25 19:00:39 +0700

    sadasdasd

--- pages/fontend/headerfont.js
+++ pages/fontend/headerfont.js
@@ -10,19 +10,19 @@ function _headerfont() {
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="robot" content="index, follow" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
         <link type="text/css" rel="stylesheet" href="/template/css/layout.css"/>
         <link type="image/ico" rel="shortcut icon" href="/template/images/favicon.ico" />
         
-        <script src="/template/js/jquery.min.js" async></script>
-        <script src="/template/js/bootstrap.bundle.js" async></script>
-        <script src="/template/js/jquery-ui.js" async></script>
-        <script src="/template/js/modernizr.js" async></script>
-        <script src="/template/js/owl.carousel.min.js" async></script>
-        <script src="/template/js/jquery.fancybox.min.js" async></script>
-        <script src="/template/js/jquery.fancybox.min.js" async></script>
-        <script type="text/javascript" src="/template/script_font/headerfont_sc.js" async></script>
+        <script src="/template/js/jquery.min.js"></script>
+        <script src="/template/js/bootstrap.bundle.js"></script>
+        <script src="/template/js/jquery-ui.js"></script>
+        <script src="/template/js/modernizr.js"></script>
+        <script src="/template/js/owl.carousel.min.js"></script>
+        <script src="/template/js/jquery.fancybox.min.js"></script>
+        <script src="/template/js/jquery.fancybox.min.js"></script>
+        <script type="text/javascript" src="/template/script_font/headerfont_sc.js"></script>
       </head>
     
     </Head>
