ac273af21071de3fc711755b082a1d3bdc35534d
ศิวกร คุณวงศ์ <notandnack@hotmail.com>
2022-08-06 13:25:04 +0700

    aaaa

--- pages/fontend/layout_font/inc_sidememberseller.js
+++ pages/fontend/layout_font/inc_sidememberseller.js
@@ -5,15 +5,15 @@ function _sidememberseller() {
   return (
     <div>
      <section id="membermenu">
-    <div class="picprofile_seller">
+    <div className="picprofile_seller">
     </div>
-    <div class="profiledetail">
+    <div className="profiledetail">
         <h3>PAPA BARKLEY</h3>
-        <li><i class="fi fi-rr-envelope"></i> papabarkley@gmail.com</li>
-        <li><i class="fi fi-rr-phone-call"></i> ********88</li>
+        <li><i className="fi fi-rr-envelope"></i> papabarkley@gmail.com</li>
+        <li><i className="fi fi-rr-phone-call"></i> ********88</li>
     </div>
 
-    <ul class="mainmember mt-3">
+    <ul className="mainmember mt-3">
         <li data-page="history"><Link href="/fontend/seller_shop"><a>รายละเอียดร้านค้า</a></Link></li>
         <li data-page="promo"><Link href="/fontend/seller_rate"><a>คะแนนร้านค้า</a></Link></li>
         <li data-page="promo"><Link href="/fontend/shop"><a>ดูร้านค้า</a></Link></li>
